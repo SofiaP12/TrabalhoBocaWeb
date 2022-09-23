2a2e2f8d9f36f8f07e55388425bc3fb7c05d92d7
SofiaP12 <pereirariosofia@gmail.com>
2022-09-23 08:54:21 -0300

    Postagem

---
+++ src/components/Search/search.jsx
@@ -0,0 +1,23 @@
+export const Search = ({busca, buscaODA}) =>{
+    return (
+        <html>
+            <head>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
+            
+            </head>
+            <body>
+            <nav className="navbar navbar-light bg-light">
+                <form className="form-inline">
+                    <input name= "busca" value= {busca} className="form-control mr-sm-2" type="search" placeholder="O que deseja buscar" aria-label="Search" onChange={buscaODA}/>
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                </form>
+            </nav>
+
+
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
+            </body>
+        </html>
+    )
+}
